db82b0b45d7fa9a6f1dce9cde4c3504599c7d8fe
BETA-TESTER-FOR-AINCRAD <sammycalixto21@gmail.com>
2025-04-28 11:56:58 -0700

    sdd
    dassda

--- Unit3Calixto/Assets/Scripts/PlayerController.cs
+++ Unit3Calixto/Assets/Scripts/PlayerController.cs
@@ -11,11 +11,13 @@ public class PlayerController : MonoBehaviour
     public float gravityModifier;
     public bool isOnGround = true;
     public bool gameOver = false;
+    private Animator playerAnim;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
 
     }
@@ -26,20 +28,22 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-           
+           isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
         }
     }
     private void OnCollisionEnter(Collision collision)
     {
        if (collision.gameObject.CompareTag("Ground"))
         {
-            isOnGround = false;
+            isOnGround = true;
         }
 
        else if (collision.gameObject.CompareTag("Obstacle"))
         {
             gameOver = true;
             Debug.Log("Game Over");
+            playerAnim.SetBool("Death_b", true);
         }
 
     }
